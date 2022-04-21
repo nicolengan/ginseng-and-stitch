79434f639923ec4e0e6cf16fe8989a5c63593dc7
nicole <nicolenganjiaxian@gmail.com>
2022-04-21 16:29:09 +0800

    test

---
+++ routes/user.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+router.get('/login', (req, res) => {
+res.render('user/login');
+});
+router.get('/register', (req, res) => {
+    res.render('user/register');
+});
+module.exports = router;
