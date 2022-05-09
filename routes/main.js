5ecbf3706490dd7e722b3411d1dfa852be187c3f
nicole <nicolenganjiaxian@gmail.com>
2022-05-09 21:39:09 +0800

    home page

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	const title = 'Video Jotter';
+	const title = 'Ginseng and Stitch';
 	// renders views/index.handlebars, passing title as an object
 	res.render('index', { title: title })
 });
