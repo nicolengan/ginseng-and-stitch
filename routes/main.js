19d71065e489854353afaa38b8685ef3f70f1817
nic <nicolenganjiaxian@gmail.com>
2022-05-18 00:02:03 +0800

    c

--- routes/main.js
+++ routes/main.js
@@ -2,13 +2,24 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	const title = 'Ginseng and Stitch';
-	// renders views/index.handlebars, passing title as an object
-	res.render('index', { title: title })
+    const title = 'Ginseng and Stitch';
+    // renders views/index.handlebars, passing title as an object
+    res.render('index', { title: title })
 });
 
 router.get('/about', (req, res) => {
-	const author = 'jqjwd';
-	res.render('about', { author });
-	});
+    res.render('about');
+});
+
+router.get('/products', (req, res) => {
+    res.render('products');
+});
+
+router.get('/workshops', (req, res) => {
+    res.render('workshops');
+});
+
+router.get('/contactUs', (req, res) => {
+    res.render('contactUs');
+});
 module.exports = router;
