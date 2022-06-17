3e8f6a7378432e8e4777214b2abf8b48263d162c
Tay Si Min <tsminxt@gmail.com>
2022-06-17 21:41:07 +0800

    x.x

--- routes/user.js
+++ routes/user.js
@@ -6,4 +6,18 @@ router.get('/login', (req, res) => {
 router.get('/register', (req, res) => {
     res.render('user/register');
 });
+
+// Classes
+router.get('/classes', (req, res) => {
+    res.render('user/classes/list');
+});
+
+router.get('/classescreate', (req, res) => {
+    res.render('user/classes/create');
+});
+
+router.get('/classesupdate', (req, res) => {
+    res.render('user/classes/update');
+});
+
 module.exports = router;
