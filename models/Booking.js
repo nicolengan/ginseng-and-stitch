0d0a352b3ab44266ac1e0a4fe1f05e1150e8cd4d
Tay Si Min <tsminxt@gmail.com>
2022-06-29 01:47:51 +0800

    Revert ":>"
    This reverts commit ecd19641b1f7a86f4abcfd1df09f9885fe7973aa.

--- models/Booking.js
+++ models/Booking.js
@@ -3,15 +3,20 @@ const db = require('../config/DBConfig');
 const moment = require('moment');
 
 // moment.suppressDeprecationWarnings = true;
-
 //booking id, course id, class id, instructor id, user id, date created
 
 // Create booking table in MySQL Database
 const Bookings = db.define('booking',
     {
+        name: { type: Sequelize.STRING(2000) },
         course_id: { type: Sequelize.INTEGER },
-        class_id: { type: Sequelize.INTEGER},
-        instructor_id: { type: Sequelize.INTEGER }
+        class_id: { type: Sequelize.INTEGER },
+        difficulty: { type: Sequelize.STRING(2000) },
+        price: { type: Sequelize.INTEGER},
+        // date: { type: Sequelize.DATE },
+        time: { type: Sequelize.TIME },
+        instructor_id: { type: Sequelize.INTEGER },
+        instructor_name: { type: Sequelize.STRING }
     }
 );
 
