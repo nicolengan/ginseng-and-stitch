03325415a8cfb0ae6261d6383f02e21118facc13
nic <nicolenganjiaxian@gmail.com>
2022-08-15 01:11:27 +0800

    added payment route for cart

--- routes/payment.js
+++ routes/payment.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Product = require('../models/Product');
 const Booking = require('../models/Booking');
 const Course = require('../models/Course');
+const Cart = require('../models/Cart');
 const stripe = require('stripe')('sk_test_51LFiQDIDLAIUfWTrXJv037R9GA5KTPZF2W98ix0WKql786N6swgCubejuSMLMIuluPGiMUyVTgp9AIz6d17fiI0T00B189hFRp');
 const YOUR_DOMAIN = 'http://localhost:5000';
 
@@ -26,6 +27,36 @@ router.get('/bookingPayment/:id', async (req, res) => {
                   name: `${x.Course.title}`,
                 },
                 unit_amount: x.Course.price * 100,
+              },
+              quantity: 1,
+            }],
+        mode: 'payment',
+        success_url: `${YOUR_DOMAIN}/booking/successful/${x.id}`,
+        cancel_url: `${YOUR_DOMAIN}`,
+    });
+
+    res.redirect(303, session.url);
+});
+
+router.get('/:id', async (req, res) => {
+    var x = await Cart.findOne({
+        where: { id: req.params.id },
+        include: { model: Product }
+    })
+    // const events = await stripe.events.list({
+    //     limit: 3,
+    //   });
+    // console.log(customers.count())
+    // console.log(events)    
+    const session = await stripe.checkout.sessions.create({
+        line_items: [
+            {
+              price_data: {
+                currency: 'SGD',
+                product_data: {
+                  name: `${x.Product.name}`,
+                },
+                unit_amount: x.Product  .price * 100,
               },
               quantity: 1,
             }],
